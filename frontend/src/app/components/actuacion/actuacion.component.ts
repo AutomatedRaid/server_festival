3a4f927fcc187e33346ceb2d46f5b2d28ba89ba7
Eduard Guillot <eduard.guillot.alvarez@cieep.com>
2021-03-24 16:17:08 +0100

    TimePicker echo

--- frontend/src/app/components/actuacion/actuacion.component.ts
+++ frontend/src/app/components/actuacion/actuacion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const M: any;
+
 @Component({
   selector: 'app-actuacion',
   templateUrl: './actuacion.component.html',
@@ -10,6 +12,7 @@ export class ActuacionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    M.AutoInit();
   }
 
 }
