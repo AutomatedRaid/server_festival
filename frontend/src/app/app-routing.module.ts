f2169701e04b22a8179400d000e12d332b4d27e5
Eduard Guillot <eduard.guillot.alvarez@cieep.com>
2021-03-24 15:43:17 +0100

    TimePicker error

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
+import {ActuacionComponent} from "./components/actuacion/actuacion.component";
 
 const routes: Routes = [
   {
     path:'',
     pathMatch: 'full',
     component: HomeComponent
+  },
+  {
+    path:'actuacion',
+    pathMatch: 'full',
+    component: ActuacionComponent
+  },
+  {
+    path:'actuacion/:id',
+    pathMatch: 'full',
+    component: ActuacionComponent
   }
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ActuacionComponent } from './components/actuacion/actuacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ActuacionComponent
   ],
   imports: [
     BrowserModule,
