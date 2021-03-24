f4c9d3273aa5d7e037a5c4583ca8256b96b98997
Eduard Guillot <eduard.guillot.alvarez@cieep.com>
2021-03-24 16:00:11 +0100

    TimePicker error

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ActuacionComponent } from './components/actuacion/actuacion.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ActuacionComponent } from './components/actuacion/actuacion.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
