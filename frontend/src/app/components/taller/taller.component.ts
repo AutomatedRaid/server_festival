fb95eed15f67bd7c63502a93cf8d39f1fb596039
David <davidaquos@gmail.com>
2021-03-29 11:25:14 +0200

    Taller / actuacion hechos

--- frontend/src/app/components/taller/taller.component.ts
+++ frontend/src/app/components/taller/taller.component.ts
@@ -1,18 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, NgForm} from "@angular/forms";
+import {Taller} from "../../models/taller";
+import axios from 'axios';
 
 declare const M: any;
 
+let imageUploader;
+const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/djlgdcqhg/image/upload'
+const CLOUDINARY_UPLOAD_PRESET = 'rdzzccwc';
 @Component({
   selector: 'app-taller',
   templateUrl: './taller.component.html',
   styleUrls: ['./taller.component.css']
 })
+
 export class TallerComponent implements OnInit {
 
-  constructor() { }
+  ngModel: Taller;
+  horaIniciov = '';
+  horaFinv = '';
+  img: string | ArrayBuffer = 'assets/img-not-found.png';
+  img2: string | ArrayBuffer = 'assets/img-not-found.png';
+  private file1: any;
+  private file2: any;
 
-  ngOnInit(): void {
-    M.AutoInit();
+  constructor() {
+    this.ngModel = new Taller();
   }
 
+  ngOnInit() {
+    imageUploader = document.getElementById('img-uploader');
+    M.AutoInit();
+    document.addEventListener('DOMContentLoaded', function () {
+      var elems = document.querySelectorAll('.timepicker');
+      var instances = M.Timepicker.init(elems, {
+        defaultTime: '9:00',
+        twelveHour: false,
+        i18n: {cancel: 'Cancelar', done: 'Aceptar'}
+      });
+    });
+  }
+
+  guardarTaller(actForm: NgForm) {
+    console.log(actForm.value);
+    console.log(this.horaIniciov);
+    console.log(this.horaFinv);
+    this.uploadimg(1);
+    this.uploadimg(2);
+  }
+
+  onSelectFile(event, number: number) {
+    if (event.target.files && event.target.files[0]) {
+      switch (number) {
+        case 1:
+          this.file1 = event.target.files[0];
+          break;
+        case 2:
+          this.file2 = event.target.files[0];
+          break;
+      }
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (eventt) => {
+        console.log(eventt);
+        switch (number) {
+          case 1:
+            this.img = eventt.target.result;
+            break;
+          case 2:
+            this.img2 = eventt.target.result;
+            break;
+        }
+      }
+    }
+  }
+
+  async uploadimg(number: number) {
+    let file: any;
+    if (number === 1) {
+      file = this.file1;
+    } else {
+      file = this.file2;
+    }
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+    const res = await axios.post(
+      CLOUDINARY_URL,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        onUploadProgress(e) {
+          let progress = Math.round((e.loaded * 100.0) / e.total);
+          console.log(progress);
+        }
+      }
+    );
+    console.log(res);
+  }
 }
--- frontend/src/app/models/taller.ts
+++ frontend/src/app/models/taller.ts
@@ -1,2 +1,18 @@
 export class Taller {
+  _id: string;
+  nombre: String;
+  horario: String;
+  descripcion: String;
+  img: String;
+  img_mapa: String;
+
+
+  constructor(id = "", nombre = "", horario = "", descripcion = "", img = "", img_mapa = "") {
+    this._id = id;
+    this.nombre = nombre;
+    this.horario = horario;
+    this.descripcion = descripcion;
+    this.img = img;
+    this.img_mapa = img_mapa;
+  }
 }
