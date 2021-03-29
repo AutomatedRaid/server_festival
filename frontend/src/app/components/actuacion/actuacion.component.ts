9ebd0969ef7b1a2d40a0900ba1d714f4abde48f2
Eduard Guillot <eduard.guillot.alvarez@cieep.com>
2021-03-29 10:35:24 +0200

    form inputs input img

--- frontend/src/app/components/actuacion/actuacion.component.ts
+++ frontend/src/app/components/actuacion/actuacion.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, NgForm} from "@angular/forms";
+import {Actuacion} from "../../models/actuacion";
 
 declare const M: any;
 
@@ -9,10 +11,68 @@ declare const M: any;
 })
 export class ActuacionComponent implements OnInit {
 
-  constructor() { }
+  ngModel: Actuacion;
+  horaIniciov = '';
+  horaFinv = '';
+  artistas: string[] = [];
+  img: string | ArrayBuffer = 'assets/img-not-found.png';
+  img2: string | ArrayBuffer = 'assets/img-not-found.png';
+
+  constructor() {
+    this.ngModel = new Actuacion();
+  }
 
   ngOnInit(): void {
     M.AutoInit();
+    document.addEventListener('DOMContentLoaded', function () {
+      var elems = document.querySelectorAll('.timepicker');
+      var instances = M.Timepicker.init(elems, {
+        defaultTime: '9:00',
+        twelveHour: false,
+        i18n: {cancel: 'Cancelar', done: 'Aceptar'}
+      });
+    });
   }
 
+  guardarActuacion(actForm: NgForm) {
+    console.log(actForm.value);
+    console.log(this.horaIniciov);
+    console.log(this.horaFinv);
+  }
+
+  addArtistas(artista: HTMLInputElement) {
+    if (artista.value != "" && artista.value != null) {
+      this.artistas.push(artista.value);
+      artista.value = "";
+      console.log(this.artistas);
+    }
+  }
+
+  removeArtistas(a: string) {
+    for (let i = 0; i < this.artistas.length; i++) {
+      if(this.artistas[i] == a){
+        this.artistas.splice(i,1);
+        console.log(this.artistas);
+        break;
+      }
+    }
+  }
+
+  onSelectFile(event, number: number) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (eventt) => {
+        console.log(eventt);
+        switch (number) {
+          case 1:
+            this.img = eventt.target.result;
+            break;
+          case 2:
+            this.img2 = eventt.target.result;
+            break;
+        }
+      }
+    }
+  }
 }
--- frontend/src/app/models/actuacion.ts
+++ frontend/src/app/models/actuacion.ts
@@ -1,2 +1,20 @@
 export class Actuacion {
+  _id: string;
+  nombre: String;
+  horario: String;
+  artistas: String[];
+  descripcion: String;
+  img: String;
+  img_mapa: String;
+
+
+  constructor(id = "", nombre = "", horario = "", artistas = [], descripcion = "", img = "", img_mapa = "") {
+    this._id = id;
+    this.nombre = nombre;
+    this.horario = horario;
+    this.artistas = artistas;
+    this.descripcion = descripcion;
+    this.img = img;
+    this.img_mapa = img_mapa;
+  }
 }
