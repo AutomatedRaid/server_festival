fd46f55661b2b0d48de8f0757c264009f131fcf9
David <davidaquos@gmail.com>
2021-04-01 10:33:13 +0200

    si

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.set('port', process.env.PORT || 3000);
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors({origin: ['http://45.84.0.19/:8100','http://45.84.0.19/:4200']}));
+app.use(cors({origin: ['http://45.84.0.19:8100','http://45.84.0.19:4200']}));
 //Routes
 app.use('/api/eventapp', require('./routes/eventapp.routes'));
 app.use('/api/adminapp', require('./routes/adminapp.routes'));
